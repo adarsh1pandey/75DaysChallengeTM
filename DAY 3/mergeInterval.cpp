24da3462db20f0e3acc81057d830c3977a1a575e
Adarsh Pandey <adarshpnd71@gmail.com>
2022-03-26 22:25:41 +0530

    done with 3rd day

---
+++ DAY 3/mergeInterval.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<n-1;i++){
+            if(i>0 && nums[i] == nums[i-1])
+                continue;
+            else{
+                int rem = -1*nums[i];
+                int l = i+1;
+                int r = n-1;
+                while(l<r){
+                    if(nums[l]+nums[r] < rem)
+                        l++;
+                    else if(nums[l]+nums[r] > rem)
+                        r--;
+                    else{
+                        ans.push_back({nums[i],nums[l],nums[r]});
+                        if(nums[l] == nums[r])
+                            break;
+                        else{
+                            int x = nums[l];
+                            int y = nums[r];
+                            while(x == nums[l])
+                                l++;
+                            while(y == nums[r])
+                                r--;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
---
+++ DAY 3/removeDuplicates.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j = 0;
+        for(int i = 1; i < nums.size(); i++)
+        {
+            if (nums[j] != nums[i])
+            {
+                j++;
+                nums[j] = nums[i];
+            }
+            
+        }
+        return j + 1;
+    }
+};
